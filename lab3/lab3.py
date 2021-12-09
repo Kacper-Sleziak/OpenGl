d8f22d5060baf347435255592f87dd39a20bca2f
Kacper <kacper-sleziak@wp.pl>
2021-12-09 23:24:13 +0100

    Lab 4 Update

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -44,12 +44,12 @@ def spin(angle):
 
 
 def get_color_array(n):
-    colors = [[(0, 0, 0) for col in range(n+1)] for row in range(n+1)]
+    colors = [[(0, 0, 0) for col in range(n + 1)] for row in range(n + 1)]
 
     random.seed(random.randint(1, 100))
 
-    for i in range(n+1):
-        for k in range(n+1):
+    for i in range(n + 1):
+        for k in range(n + 1):
             r = random.uniform(0, 1)
             g = random.uniform(0, 1)
             b = random.uniform(0, 1)
@@ -59,15 +59,13 @@ def get_color_array(n):
 
 
 def get_points(n):
-    array_of_points = [[(0, 0, 0) for col in range(n+1)] for row in range(n+1)]
+    array_of_points = [[(0, 0, 0) for col in range(n + 1)] for row in range(n + 1)]
     step = 1 / n
 
     for i in range(n + 1):
         for k in range(n + 1):
             u = 0 + i * step
             v = 0 + k * step
-
-            print(f"u:{u}, v: {v}")
 
             x = (-90 * u ** 5 + 225 * u ** 4 - 270 * u ** 3 + 180 * u ** 2 - 45 * u) * math.cos(math.pi * v)
             y = 160 * u ** 4 - 320 * u ** 3 + 160 * u ** 2
@@ -86,48 +84,45 @@ def render(time, points, colors, n):
     angle = time * 180 / math.pi
     spin(angle)
 
-    for i in range(n+1):
-        for j in range(n+1):
-            if i + 1 < n+1 and j + 1 < n+1:
-                for k in range(3):
-                    x1, y1, z1 = points[i][j]
-                    x2, y2, z2 = points[i + 1][j]
-                    x3, y3, z3 = points[i][j + 1]
-                    x4, y4, z4 = points[i + 1][j + 1]
+    for i in range(n + 1):
+        for j in range(n + 1):
+            if i + 1 < n + 1 and j + 1 < n + 1:
 
-                    glBegin(GL_TRIANGLES)
+                x1, y1, z1 = points[i][j]
+                x2, y2, z2 = points[i + 1][j]
+                x3, y3, z3 = points[i][j + 1]
+                x4, y4, z4 = points[i + 1][j + 1]
 
-                    r, g, b = colors[i][j]
-                    glColor3f(r, g, b)
-                    glVertex3f(x1, y1, z1)
+                glBegin(GL_TRIANGLES)
 
-                    r, g, b = colors[i + 1][j]
-                    glColor3f(r, g, b)
-                    glVertex3f(x2, y2, z2)
+                r, g, b = colors[i][j]
+                glColor3f(r, g, b)
+                glVertex3f(x1, y1, z1)
 
-                    r, g, b = colors[i][j+1]
-                    glColor3f(r, g, b)
-                    glVertex3f(x3, y3, z3)
-                    glEnd()
+                r, g, b = colors[i + 1][j]
+                glColor3f(r, g, b)
+                glVertex3f(x2, y2, z2)
 
+                r, g, b = colors[i][j + 1]
+                glColor3f(r, g, b)
+                glVertex3f(x3, y3, z3)
+                glEnd()
 
-                    glBegin(GL_TRIANGLES)
+                glBegin(GL_TRIANGLES)
 
-                    r, g, b = colors[i + 1][j]
-                    glColor3f(r, g, b)
-                    glVertex3f(x2, y2, z2)
+                r, g, b = colors[i + 1][j]
+                glColor3f(r, g, b)
+                glVertex3f(x2, y2, z2)
 
-                    r, g, b = colors[i][j + 1]
-                    glColor3f(r, g, b)
-                    glVertex3f(x3, y3, z3)
+                r, g, b = colors[i][j + 1]
+                glColor3f(r, g, b)
+                glVertex3f(x3, y3, z3)
 
-                    glVertex3f(x4, y4, z4)
-                    r, g, b = colors[i + 1][j +1]
-                    glColor3f(r, g, b)
-                    glEnd()
-
+                glVertex3f(x4, y4, z4)
+                r, g, b = colors[i + 1][j + 1]
+                glColor3f(r, g, b)
+                glEnd()
     axes()
-
     glFlush()
 
 
@@ -165,7 +160,7 @@ def main():
     glfwSwapInterval(1)
 
     startup()
-    n = 20
+    n = 50
     points = get_points(n)
     colors = get_color_array(n)
     while not glfwWindowShouldClose(window):
